7fbf24bbfed217382e380cfe38708f80c8c44c83
Aadhith <aadhith@asherdn.com>
2026-06-28 09:30:59 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-900">
+      <h1 className="text-4xl font-bold text-teal-400 drop-shadow-md">
+        React + Tailwind Setup Successful!
+      </h1>
+    </div>
+  )
+}
+
+export default App
